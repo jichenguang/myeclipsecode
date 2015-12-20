7e9f0d1a28d69a38400433bf67c6ab62055b1518
jichenguang <jichenguang@700bike.com>
2015-12-20 23:25:13 +0800

    hi

---
+++ src/test/com/SimpleGuil.java
@@ -0,0 +1,20 @@
+package test.com;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+
+public class SimpleGuil {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		JFrame frame = new JFrame();
+		JButton button = new JButton("click me");
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().add(button);
+		frame.setSize(300, 300);
+		frame.setVisible(true);
+		
+
+	}
+
+}
